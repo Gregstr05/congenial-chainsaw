a875eda377bca41d9a42e8273d9b2dc45c3e5236
Adam Gregořica <44508744+Gregstr05@users.noreply.github.com>
2022-12-10 19:16:07 +0100

    added favicon

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="https://www.postavy.cz/foto/walter-white-foto.jpg\">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Čtvrtý reaktor</title>
+</head>
+
 <?php
 function getUserIP()
 {
